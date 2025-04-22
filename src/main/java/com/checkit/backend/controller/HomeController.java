c49ec5cd8ad7f7f195d6312ad98b4b544b86f6ed
SeungRyeol Lim <tmdfuf1201@naver.com>
2025-04-23 04:09:12 +0900

    feat: HomeController 수정

--- src/main/java/com/checkit/backend/controller/HomeController.java
+++ src/main/java/com/checkit/backend/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    
     @GetMapping("/")
     public APIResponse<?> home(){
         return APIResponse.success("Welcome to Check-It!");
