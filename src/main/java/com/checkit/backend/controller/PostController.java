146af8966845862cc82cb563c6070c3c4ac1b8b1
SeungRyeol Lim <tmdfuf1201@naver.com>
2025-05-02 10:18:48 +0900

    feat: PostController api 기본 세팅

--- src/main/java/com/checkit/backend/controller/PostController.java
+++ src/main/java/com/checkit/backend/controller/PostController.java
@@ -1,12 +1,36 @@
 package com.checkit.backend.controller;
 
+import com.checkit.backend.global.response.APIResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/post")
 @RequiredArgsConstructor
 public class PostController {
 
+    @PostMapping("")
+    public APIResponse<String> uploadPost(){
+        return APIResponse.success("업로드 성공");
+    }
+
+    @GetMapping("")
+    public APIResponse<?> getAllPost(){
+        return APIResponse.success("");
+    }
+
+    @GetMapping("")
+    public APIResponse<?> getPost(){
+        return APIResponse.success("");
+    }
+
+    @PatchMapping("/{id}")
+    public APIResponse<?> updatePost(){
+        return APIResponse.success("");
+    }
+
+    @DeleteMapping("/{id}")
+    public APIResponse<?> deletePost(){
+        return APIResponse.success("");
+    }
 }
